2e3db29a19cd5f093700343a87e7b8f10d22b024
Oluwaseye Ayooluwa Philip <depatez@gmail.com>
2024-12-05 03:13:03 -0800

    prod(calculator): first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// // script to enable click to display
+// const btn = document.querySelectorAll('btn');
+
+// btn.forEach(link => {
+//     targetValues.addEventListener('click', function(event)=>{
+//         event.preventDefault();
+//         const targetDisplay = document.querySelector('display')
+//         targetDisplay.style.display='block';
+//     });
+// });
